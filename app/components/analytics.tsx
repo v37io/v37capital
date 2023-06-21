fb797a49ae3e4488967a87078556caf202c496b4
Daniele Salatti <me@danielesalatti.com>
2023-06-21 16:37:58 -0700

    add zipline description

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,15 +1,13 @@
 "use client";
 
 export function Analytics() {
-	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
-	if (!token) {
-		return null;
-	}
-	return (
-		<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token={token}
-			async
-		/>
-	);
+  return (
+    <script
+      async
+      defer
+      data-domain="v37.capital"
+      data-api="https://scriba.scopecreep.workers.dev/w3api/event"
+      src="https://scriba.scopecreep.workers.dev/w3js/script.outbound-links.js"
+    ></script>
+  );
 }
